f02fe7019016ea4a134ce18c1d0a1b0b6219fce6
Diego Martinez <dmartinez-2026166@kinal.edu.gt>
2026-08-25 13:13:04 -0600

    fix: union de problema 23 y 29

--- src/com/kennethvelasquez/system/Main.java
+++ src/com/kennethvelasquez/system/Main.java
@@ -6,30 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        estructuraBaseEjercicio23();
         // Cada alumno puede llamar temporalmente su método aquí para probarlo.
         // Ejemplo:
         // estructuraBaseEjercicio1();
         estructuraBaseEjercicio29();
     }
 
-    /**
-     * Problema 29: Define una nota entre 0 y 100 y muestra su clasificación:
-     *
-     * 90 - 100 = Excelente 80 - 89 = Muy bueno 70 - 79 = Bueno 60 - 69 =
-     * Aprobado 0 - 59 = Reprobado
-     *
-     * Al resolver el problema y comprobar que ejecuta correctamente, borra esta
-     * sección del comentario.
-     *
-     * git add Main.java git commit -m "feat: resolver problema 29 clasificar
-     * nota" git push origin rama_destino
-     *
+  
+    public static void estructuraBaseEjercicio22() {
 
-     * gh pr create --base tu-rama --head rama_destino \ --title "Resolver
-     * problema 29" \ --body "Se implementó una estructura condicional para
-     * clasificar una nota según diferentes rangos."
-     */
-    public static void estructuraBaseEjercicio29() {
+    }
+    
+    public static void estructuraBaseEjercicio23() {
+        int[] numeros = {10, 25, 30, 42, 55};
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+
+  public static void estructuraBaseEjercicio29() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese su Calificacion: ");
 
