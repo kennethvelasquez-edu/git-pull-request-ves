9141ff00a4af5d39d8b21467c68a6ffad07e9364
Jeison Garcia <informatica@alumno.local>
2026-08-25 13:35:43 -0600

    feat : estructuraBaseEjercicio12

--- src/com/kennethvelasquez/system/Main.java
+++ src/com/kennethvelasquez/system/Main.java
@@ -1,5 +1,7 @@
 package com.kennethvelasquez.system;
 
+import java.util.Scanner;
+
 
 
 public class Main {
@@ -263,8 +265,57 @@ public class Main {
      * --title "Resolver problema 12" \
      * --body "Se implementó la lógica para obtener el nombre de un mes a partir de su número."
      */
+    
+    
     public static void estructuraBaseEjercicio12() {
 
+        Scanner lector = new Scanner(System.in);
+        
+        System.out.print("ingrese un número del 1 al 12 para saber que mes es ");
+        
+        int numero = lector.nextInt();
+        
+        switch (numero) {
+            case 1:
+                System.out.println("Enero");
+                break;
+            case 2:
+                System.out.println("Febrero");
+                break;
+            case 3:
+                System.out.println("Marzo");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Mayo");
+                break;
+            case 6:
+                System.out.println("Junio");
+                break;
+            case 7:
+                System.out.println("Julio");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Septiembre");
+                break;
+            case 10:
+                System.out.println("Octubre");
+                break;
+            case 11:
+                System.out.println("Noviembre");
+                break;
+            case 12:
+                System.out.println("Diciembre");
+                break;
+            default:
+                System.out.println("El número debe estar entre 1 y 12.");
+                break;
+        }
     }
 
 
